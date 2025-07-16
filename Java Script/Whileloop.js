df548ce6d772f3917fbbd956f7d5482340668c7c
Sugumar0411 <sugumarsugu0411@gmail.com>
2025-07-16 10:34:39 +0530

    data added

---
+++ Java Script/Whileloop.js
@@ -0,0 +1,12 @@
+let i=1;
+while(i<=5)//condition is known
+{
+    console.log("The use of while loop,i");
+    i++;
+}
+let x=6
+do
+{
+    console.log("number is",i);
+    i++;
+}while(i<=5);/condition at the last
---
+++ Java Script/basic.js
@@ -0,0 +1,17 @@
+/*declear a variable */
+var name"SLA";
+/*
+1.function-scpoud
+2.can be re-decleared
+
+
+
+
+
+
+
+
+const a=10;
+const b=20;
+const sum=a+b;
+
